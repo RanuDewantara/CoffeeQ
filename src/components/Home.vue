1a8b401561a889c65f5f4ff75b2ca753066867e7
RanuDewantara <ranuputradewantara@programmer.net>
2025-04-05 19:59:20 +0700

    update: fix home text

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,10 +1,6 @@
 <template>
-<!-- 	<div id="home" class="relative min-h-fit bg-[url('../src/assets/img/coffee.jpg')] bg-[auto_250px] bg-no-repeat bg-center mt-22 py-20">-->
-<!-- 		<h1 class="font-['Oxanium'] font-bold text-4xl text-center text-white animate-fade-in-scale md:text-5xl lg:text-6xl 2xl:text-7xl">Start your day with <span class="text-amber-300">coffee</span></h1>-->
-<!--	</div>-->
-	
 	<div id="home" class="relative pt-20 bg-yellow-950">
 		<img src="../assets/img/coffee2.jpg" alt="Coffee Logo" class="max-h-screen mx-auto border-8 border-yellow-950" />
-		<h1 class="absolute top-1/2 left-0 right-0 mx-auto font-['Oxanium'] font-bold text-4xl text-center text-white animate-fade-in-scale md:text-5xl lg:text-6xl 2xl:text-7xl">Start your day with <span class="text-amber-300">coffee</span></h1>
+		<h1 class="absolute top-1/2 left-0 right-0 mx-auto font-['Oxanium'] font-bold text-4xl text-center text-white animate-fade-in-scale md:text-6xl 2xl:text-7xl">Start your day with <span class="text-amber-300">coffee</span></h1>
 	</div>
 </template>
