93e51995fcddaf7f9da519c94b24c8ed52795342
RanuDewantara <ranuputradewantara@programmer.net>
2025-04-05 20:21:50 +0700

    fix: Home and About

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,6 +1,6 @@
 <template>
-	<div id="home" class="relative pt-20 bg-yellow-950">
-		<img src="../assets/img/coffee2.jpg" alt="Coffee Logo" class="max-h-screen mx-auto border-8 border-yellow-950" />
-		<h1 class="absolute top-1/2 left-0 right-0 mx-auto font-['Oxanium'] font-bold text-4xl text-center text-white animate-fade-in-scale md:text-6xl 2xl:text-7xl">Start your day with <span class="text-amber-300">coffee</span></h1>
+	<div id="home" class="relative pt-20 pb-10 bg-yellow-950">
+		<img src="../assets/img/coffee2.jpg" alt="Coffee Logo" class="max-h-lvh mx-auto border-8 border-yellow-950" />
+		<h1 class="absolute top-1/2 left-0 right-0 mx-auto font-['Oxanium'] font-bold text-[2.8rem] text-center text-yellow-100 leading-10  animate-fade-in-scale md:text-[3.5rem] 2xl:text-7xl">Start your day with <span class="text-amber-300">coffee</span></h1>
 	</div>
 </template>
